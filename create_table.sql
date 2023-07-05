a18332fc59fde0b47594da778f0dbf180fa3f00c
utof0x <popovychmykhailo00@gmail.com>
2023-07-05 23:32:40 +0300

    created file to create users table

---
+++ create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sql_login`.`new_table` (
+ `id` INT NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(255) NOT NULL,
+ `username` VARCHAR(255) NOT NULL,
+ `email` VARCHAR(255) NOT NULL,
+ `password` VARCHAR(255) NOT NULL,
+ PRIMARY KEY (`id`));
